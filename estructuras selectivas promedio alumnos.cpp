922cfe88aeb7663ecc7a51bf7dcce6b22b0a18cd
eber <eber.penaloza10@gmail.com>
2024-06-15 21:03:24 -0700

    mi primer ejercicio de estruturas selectivas

---
+++ estructuras selectivas promedio alumnos.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+double promedio, igv, descuento, pension, pagofinal;
+cout<<"Ingrese su promedio: "; cin>>promedio;
+cout<<"Ingrese la pension educativa: "; cin>>pension;
+if(promedio>=18)
+{
+descuento=(pension*0.3);
+igv=0;
+}
+else
+{
+descuento=0;
+igv=(pension*0.18);
+}
+pagofinal=((pension-descuento)+igv);
+cout<<"Su pension a pagar es: S/. " <<pagofinal;
+return 0;
+}
