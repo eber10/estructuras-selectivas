ad32171475e0dcbf216f72b3e11e9d94241fb46f
eber <eber.penaloza10@gmail.com>
2024-06-17 17:47:49 -0700

    menú agregado

--- estructuras selectivas promedio alumnos.cpp
+++ estructuras selectivas promedio alumnos.cpp
@@ -4,25 +4,43 @@
 using namespace std;
 int main()
 {
+	int op;
     double promedio, igv, descuento, pension, pagofinal;
     string user, clave;
     cout<<"BIENVENIDO AL SISTEMA "<<endl;
     cout<<"**********************"<<endl;
     cout<<"USUARIO: "; cin>>user;
     cout<<"CLAVE: "; cin>>clave;
-    cout<<"Ingrese su promedio: "; cin>>promedio;
-    cout<<"Ingrese la pension educativa: "; cin>>pension;
-    if(promedio>=18)
+    do
     {
-        descuento=(pension*0.3);
-        igv=0;
-    }
-    else
-    {
-        descuento=0;
-        igv=(pension*0.18);
-    }
-    pagofinal=((pension-descuento)+igv);
-    cout<<"Su pension a pagar es: S/. " <<pagofinal;
+    	cout<<"1. pension "<<endl;
+    	cout<<"2. promedio "<<endl;
+    	cout<<"0. salir "<<endl;
+    	cin>>op;
+    	switch(op)
+    	{
+    		case 1:
+    			cout<<"Ingrese su promedio: "; cin>>promedio;
+                cout<<"Ingrese la pension educativa: "; cin>>pension;
+                if(promedio>=18)
+                {
+                    descuento=(pension*0.3);
+                    igv=0;
+                }
+                else
+                {
+                    descuento=0;
+                    igv=(pension*0.18);
+                }
+                pagofinal=((pension-descuento)+igv);
+                cout<<"Su pension a pagar es: S/. " <<pagofinal<<endl;
+            case 2:
+            	break;
+            default: 
+            	break;
+		}
+	}
+	while(op!=0);
+    
     return 0;
 }
